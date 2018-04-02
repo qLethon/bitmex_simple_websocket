cef65ad4e0f1761b5398ca50626c58cbc76537b1
qLethon <qLethon@gmail.com>
2018-04-02 16:41:22 +0900

    second commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = ["requests>=2.14.2"]
 
 
 setup(
-    name='bitmex_simple_ws',
+    name='bitmex_simple_websocket',
     version='0.1',
     description='BitMEX simple websocket client',
     url='https://github.com/qLethon/bitmex_simple_ws',
@@ -13,7 +13,7 @@ setup(
     author_email='qLethon@gmail.com',
     license='MIT',
     packages=[
-        "bitmex_simple_ws",
+        "bitmex_simple_websocket",
     ],
     install_requires=[
         'websocket-client==0.46.0',
